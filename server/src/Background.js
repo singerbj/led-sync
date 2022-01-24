6a1606cfac5b3b44a6012800da22c0debfbf29a8
Benjamin Singer <benjaminjsinger@gmail.com>
2022-01-24 13:12:22 -0600

    out of focus message

--- server/src/Background.js
+++ server/src/Background.js
@@ -7,6 +7,15 @@ import { useEffect, useState } from 'react';
 const Background = ({ children }) => {
     const [connected, setConnected] = useState(false);
     const [colorState, setColorState] = useState([0, 0, 0]);
+    const [hasFocus, setHasFocus] = useState(document.hasFocus());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {  //assign interval to a variable to clear it.
+            setHasFocus(document.hasFocus());
+        }, 500)
+        
+        return () => clearInterval(intervalId); 
+    }, []);
 
     useEffect(() => {
         // let socket;
@@ -60,6 +69,9 @@ const Background = ({ children }) => {
         <div id="bkrd" style={{position: 'absolute', width: '100%', height: '100%', padding: '10px', backgroundColor: "rgb(" + colorState.join(',') + ")"}}>
             <h3 style={{ display: 'inline', color: "#000", backgroundColor: "#FFF" }}>{connected ? "Connected :)" : "Not Connected :("}</h3>
             {children}
+            <br />
+            <br />
+            {!hasFocus && <h5 style={{ display: 'inline', color: "#000", backgroundColor: "#FFF" }}>Click to recieve color updates</h5>}
         </div>
     );
 }
