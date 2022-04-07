3941675e9ea8cf912c6cefb0c7b5e8695107d330
Benjamin Singer <benjaminjsinger@gmail.com>
2022-04-06 20:43:29 -0500

    better process deaths

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
 const spawn = require("child_process").spawn;
-const pythonProcess = spawn('python3', ["server.py"]);
+const pythonProcess = spawn("python3", ["server.py"]);
 
-pythonProcess.stdout.on('data', (buffer) => {
-    console.log(buffer.toString('utf8'));
+pythonProcess.stdout.on("data", (buffer) => {
+  console.log(buffer.toString("utf8"));
 });
 
-pythonProcess.stderr.on('data', (buffer) => {
-    console.error(buffer.toString('utf8'));
+pythonProcess.stderr.on("data", (buffer) => {
+  console.error(buffer.toString("utf8"));
 });
 
-var cleanExit = function() { 
-    pythonProcess.kill();
-    process.exit()
+var cleanExit = function () {
+  pythonProcess.kill();
+  process.exit();
 };
 
-process.on('SIGINT', cleanExit); // catch ctrl-c
-process.on('SIGTERM', cleanExit);
-process.on('SIGQUIT', cleanExit);
+process.on("SIGINT", cleanExit); // catch ctrl-c
+process.on("SIGTERM", cleanExit);
+process.on("SIGQUIT", cleanExit);
+
+process.on("exit", cleanExit);
